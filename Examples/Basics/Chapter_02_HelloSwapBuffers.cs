89eb657d0a631950efb706c8917568173484dd60
doYourCode <caio.lamas@ifnmg.edu.br>
2023-04-03 15:12:11 -0300

    Atualização de documentação
    - Explicação em detalhes até o exemplo 3

--- Examples/Basics/Chapter_02_HelloSwapBuffers.cs
+++ Examples/Basics/Chapter_02_HelloSwapBuffers.cs
@@ -4,6 +4,9 @@ using OpenTK.Windowing.Desktop;
 
 namespace _3dCG.Examples.Basics
 {
+    /// <summary>
+    /// Demonstra como limpar e dispor um Frame Buffer, utilizando a funcionalidade disponível na biblioteca OpenTK.
+    /// </summary>
     internal class Chapter_02_HelloSwapBuffers : GameWindow
     {
 
@@ -12,18 +15,23 @@ namespace _3dCG.Examples.Basics
             Title = "Hello Swap Buffers!";
         }
 
+        // O método 'OnLoad' é chamado uma vez, logo após a criação da janela. Sua função é permitir que o usuário configure a janela, os objetos e carregue os dados necessários antes de iniciar o loop de renderização.
         protected override void OnLoad()
         {
             base.OnLoad();
 
+            // Configura a cor de fundo do Frame Buffer.
             GL.ClearColor(1.0f, 0.0f, 0.0f, 1.0f);
         }
 
+        /// O método 'OnRenderFrame' é chamado a cada frame, logo após atualizar a lógica de negócio. Sua função é permitir que o usuário desenhe na tela, utilizando os dados disponíveis.
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
 
+            // Limpa o Frame Buffer, utilizando a cor configurada anteriormente.
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            // Disponibiliza o Frame Buffer na tela, alternando entre dois buffers que são pintados em sequência (Double buffering).
             SwapBuffers();
         }
     }
--- Examples/Basics/Chapter_03_HelloTriangle.cs
+++ Examples/Basics/Chapter_03_HelloTriangle.cs
@@ -1,12 +1,18 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using System.ComponentModel;
+using System;
 
 namespace _3dCG.Examples.Basics
 {
+    /// <summary>
+    /// Demonstra como desenhar um triângulo na tela, carregar um buffer de dados na memória de video, e fazer uma chamada de desenho utilizando a funcionalidade disponível na biblioteca OpenTK.
+    /// </summary>
     internal class Chapter_03_HelloTriangle : GameWindow
     {
 
+        // TODO: Criar uma classe para gerenciar os buffers de dados, e permitir que o usuário configure os atributos de cada buffer.
         private const int POSITION = 0;
         private readonly int[] OFFSET = { 0 };
         private const int VERTEX_SIZE = 3 * sizeof(float);
@@ -23,47 +29,60 @@ namespace _3dCG.Examples.Basics
         {
             base.OnLoad();
 
+            // Dados que serão carregados no buffer (por hora só temos as posições dos vértices de um triângulo)
             float[] _data =
             {
-                -0.75f, -0.75f, 0.0f,
-                0.75f , -0.75f, 0.0f,
-                0.0f  , 0.75f , 0.0f
+                // Posições
+                -0.75f, -0.75f, 0.0f, // Vértice 0 -> canto inferior esquerdo
+                0.75f , -0.75f, 0.0f, // Vértice 1 -> canto inferior direito
+                0.0f  , 0.75f , 0.0f  // Vértice 2 -> canto superior (no centro da tela)
             };
 
-            // Generate the buffer
-            _vertexBufferObject = GL.GenBuffer();
-            // Points to the active buffer
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            // Insert the data into the buffer
-            GL.BufferData(BufferTarget.ArrayBuffer, _data.Length * sizeof(float), _data, BufferUsageHint.StaticDraw);
-            // Generate the array object buffer
+            // Gerar o buffer do objeto array de vértices (VAO) e armazenar o identificador do buffer na variável _vertexArrayObject (VAO)
+            // O VAO é um objeto que armazena o estado de todos os atributos de um buffer de vértices (VBO)
+            // Você pode criar o VAO antes ou depois dos VBOs, mas ambos devem ser criados antes de definir os dados de atributo. Um VAO é simplesmente um contêiner para o estado relacionado (ou seja, as fontes de dados para cada atributo, além de uma matriz de elementos)
             _vertexArrayObject = GL.GenVertexArray();
-            // Points to the array object
+
+            // Aponta para o objeto array de vértices (VAO) que está ativo no momento (o objeto array de vértices que está ativo é o que foi armazenado na variável _vertexArrayObject)
             GL.BindVertexArray(_vertexArrayObject);
-            // Creates an attribute pointer
+
+            // Gerar o objeto buffer de vértices (VBO) e armazenar o identificador do buffer na variável _vertexBufferObject
+            _vertexBufferObject = GL.GenBuffer();
+
+            // Ativa o buffer para ser alterado pelas próximas chamadas (Isso é necessário pois o opengl é como uma grande maquina de estados, e para cada chamada é necessário informar o estado atual)
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            // Carrega os dados no buffer de vértices (VBO) que está ativo no momento (o buffer de vértices que está ativo é o que foi armazenado na variável _vertexBufferObject)
+            GL.BufferData(BufferTarget.ArrayBuffer, _data.Length * sizeof(float), _data, BufferUsageHint.StaticDraw);
+
+            // Cria e ativa um ponteiro de atributo associado ao buffer de vértices (VBO) que está ativo no momento
             GL.VertexAttribPointer(POSITION, 3, VertexAttribPointerType.Float, false, VERTEX_SIZE, OFFSET[POSITION]);
             GL.EnableVertexAttribArray(POSITION);
 
             GL.ClearColor(1.0f, 0.0f, 0.0f, 1.0f);
         }
-
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
-
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            // Ativa o VAO para ser desenhado
             GL.BindVertexArray(_vertexArrayObject);
+            // Chamada à função Draw
             GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             GL.BindVertexArray(0);
 
             SwapBuffers();
         }
 
+        /// <summary>
+        /// Apaga os dados carregados na memória de vídeo
+        /// </summary>
         protected override void OnUnload()
         {
             base.OnUnload();
 
+            // A memória de vídeo (que recebe os dados dos buffers) não é gerenciada pela máquina virtual da linguagem, dessa forma é necessário o programador apagar os dados quando eles não forem mais necessários.
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.BindVertexArray(0);
             GL.DeleteBuffer(_vertexBufferObject);

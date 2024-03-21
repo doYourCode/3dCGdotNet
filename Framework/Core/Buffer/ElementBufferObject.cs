c2731d91301b1539f783af650d5c624bf14636a0
Caio Henriques Sica Lamas <111379877+doYourCode@users.noreply.github.com>
2024-03-21 18:56:29 -0300

    Update EBO

--- Framework/Core/Buffer/ElementBufferObject.cs
+++ Framework/Core/Buffer/ElementBufferObject.cs
@@ -2,20 +2,50 @@
 
 namespace Framework.Core.Buffer
 {
-    public struct ElementBufferObject
+    /// <summary>
+    /// Representa um buffer contendo os dados de índices para descrição de pontos, linhas, triângulos ou
+    /// elementos geométricos semelhantes. Esse tipo de buffer será utilizado para desenhar utilizando a
+    /// função GL.DrawElements(...) cujos índices usados para renderização indexada serão retirados do
+    /// ElementBufferObject (EBO).
+    /// <br />
+    /// ATENÇÃO: esse objeto deve ser sempre vinculado a um VertexArrayObject (VAO), portanto uma chamada à
+    /// função Bind() do VAO pretendido deve ser realizada antes de vincular o novo EBO.
+    /// </summary>
+    public class ElementBufferObject
     {
-        int Handle;
+        /* ---------------------------------------------- Variáveis membro ---------------------------------------------- */
 
+        /// <summary>
+        /// Id que reflete o endereço do buffer na VRAM
+        /// </summary>
+        public UInt32 ID;
+
+
+        /* ---------------------------------------------- Interface pública ---------------------------------------------- */
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="indices"> Array de índices contendo os dados a serem enviados ao buffer.¹²
+        /// <br />
+        /// ATENÇÃO: ¹ certifique-se de que o array não é nulo. ² Na implementação atual só é permitido utilizar
+        /// números inteiros como índices.
+        /// </param>
+        /// <param name="usage"> Indicativo de para que os dados do buffer serão usados.
+        /// <br />
+        /// Valores comuns: BufferUsageHint.StaticDraw / BufferUsageHint.DynamicDraw / BufferUsageHint.StreamDraw.
+        /// Há outros valores possíveis, verifique as referências da API.
+        /// </param>
         public ElementBufferObject(int[] indices, BufferUsageHint usage = BufferUsageHint.StaticDraw)
         {
-            Handle = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Handle);
+            ID = (UInt32)GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ID);
             GL.BufferData(BufferTarget.ElementArrayBuffer, sizeof(int) * indices.Length, indices, usage);
         }
 
         public void Bind()
         {
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Handle);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ID);
         }
 
         public void Unbind()
@@ -26,7 +56,7 @@ namespace Framework.Core.Buffer
         public void Delete()
         {
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-            GL.DeleteBuffer(Handle);
+            GL.DeleteBuffer(ID);
         }
     }
 }
